f7fdb2dff9fd1d86022c3c73161440e2a3c59824
twaasoulElm3refa <twaslm3rfa@gmail.com>
2026-06-15 16:48:03 +0300

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-      './resources/**/*.blade.php',
-      './resources/**/*.js',
-      './resources/**/*.vue',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
+
     theme: {
-      extend: {
-        fontFamily: {
-          sans: [
-            'Instrument Sans',
-            'ui-sans-serif',
-            'system-ui',
-            'sans-serif',
-            'Apple Color Emoji',
-            'Segoe UI Emoji',
-            'Segoe UI Symbol',
-            'Noto Color Emoji',
-          ],
+        extend: {
+            fontFamily: {
+                camel: ['Camel', 'sans-serif'],
+                sans: ['Camel', 'sans-serif'],
+            },
         },
-      },
     },
+
     plugins: [],
-  }
+}
