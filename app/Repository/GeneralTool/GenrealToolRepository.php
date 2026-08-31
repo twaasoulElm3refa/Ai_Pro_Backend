df63ce260a3735fc438b95d1e7f89e182edc2826
twaasoulElm3refa <twaslm3rfa@gmail.com>
2026-08-31 14:04:14 +0300

    ..

--- app/Repository/GeneralTool/GenrealToolRepository.php
+++ app/Repository/GeneralTool/GenrealToolRepository.php
@@ -8,7 +8,19 @@ class GenrealToolRepository implements GenrealToolInterface
 {
     public function getAll()
     {
-        return SubTools::where('main_tool_id', 8)->get();
+        return SubTools::where('main_tool_id', 8)
+            ->select([
+                'id',
+                'main_tool_id',
+                'name',
+                'meta_name',
+                'slug',
+                'prompt_placeholder',
+                'is_active',
+                'tier',
+                'is_free',
+            ])
+            ->get();
     }
 
     public function show($id)
