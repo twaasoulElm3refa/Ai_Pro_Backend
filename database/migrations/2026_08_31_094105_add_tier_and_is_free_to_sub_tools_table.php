30797464a7d77f5315d55e06aafc727bbd22d25c
twaasoulElm3refa <twaslm3rfa@gmail.com>
2026-08-31 12:56:16 +0300

    add new columns

--- database/migrations/2026_08_31_094105_add_tier_and_is_free_to_sub_tools_table.php
+++ database/migrations/2026_08_31_094105_add_tier_and_is_free_to_sub_tools_table.php
@@ -6,23 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('sub_tools', function (Blueprint $table) {
-            //
+            $table->string('tier')->nullable();
+            $table->boolean('is_free')->default(false)->after('tier');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('sub_tools', function (Blueprint $table) {
-            //
+            $table->dropColumn([
+                'tier',
+                'is_free',
+            ]);
         });
     }
 };
