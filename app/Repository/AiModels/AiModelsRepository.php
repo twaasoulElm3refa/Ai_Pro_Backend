9fc931db0a4f25724c44f8f3d870cab8eee1f446
twaasoulElm3refa <twaslm3rfa@gmail.com>
2026-09-17 13:57:25 +0300

    fix error

--- app/Repository/AiModels/AiModelsRepository.php
+++ app/Repository/AiModels/AiModelsRepository.php
@@ -8,6 +8,6 @@ class AiModelsRepository implements AiModelsInterface
 {
     public function AiModels()
     {
-        return MainFreeAiModels::with('translation')->take(5)->get();
+        return MainFreeAiModels::with('translation')->take(5)->select(['id', 'slug'])->get();
     }
 }
