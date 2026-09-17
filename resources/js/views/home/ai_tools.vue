a363da2f225bdecb52f03406dee1393e331b4317
twaasoulElm3refa <twaslm3rfa@gmail.com>
2026-09-17 13:55:43 +0300

    fix error

--- resources/js/views/home/ai_tools.vue
+++ resources/js/views/home/ai_tools.vue
@@ -6,14 +6,4 @@
 
 
 <script setup>
-import { computed, onMounted, onUnmounted, ref, watch } from "vue";
-import { useRoute, useRouter } from "vue-router";
-import { useI18n } from "vue-i18n";
-import aiModelsService from "@/services/AiModels/toolServices";
-import useSeoMeta from "@/composables/useSeoMeta";
-
-const route = useRoute();
-const router = useRouter();
-const { t, locale } = useI18n();
-
 </script>
