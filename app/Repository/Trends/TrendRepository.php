2c71b516899025d0706fbf0a3c5f4a374950ba28
MohamedMaherTawfik <m7mdellham77@gmail.com>
2026-09-19 10:00:26 +0300

    edit fetch

--- app/Repository/Trends/TrendRepository.php
+++ app/Repository/Trends/TrendRepository.php
@@ -10,6 +10,6 @@ class TrendRepository implements TrendInterface
     {
         return SubTools::whereIn('id', [41, 42, 43, 44, 45])
             ->with('translation:name,sub_tools_id,id,locale,description')
-            ->select();
+            ->select(['id', 'name', 'description', 'slug']);
     }
 }
